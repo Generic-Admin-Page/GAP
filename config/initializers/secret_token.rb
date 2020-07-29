c88a0e9de88542e6a7e0e64c10e24d05d9be71ca
Generic-Admin-Page <68966953+Generic-Admin-Page@users.noreply.github.com>
2020-07-29 14:22:22 -0400

    add some sooper sensitive files so i don't lose em!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GAP::Application.config.secret_token = '975ecbc3fd0cc30cec2ed9eb18bcbd9c5e82475192e17a1ac7bf47c52b78115aa81b97a214e772d742ffb1c607285eeca8132965e07fc3447415d'
